b4a641e759a7e52f5fe7cd0fa7404194937ae057
Lokeshwaraprasad <venkatesanlokeshwaraprasad@gmail.com>
2024-05-13 12:33:02 +0530

    Counting valley #Hackerrank.c

---
+++ countingvalley.c
@@ -0,0 +1,287 @@
+/*
+An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+
+A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+Example
+
+ 
+
+The hiker first enters a valley  units deep. Then they climb out and up onto a mountain  units high. Finally, the hiker returns to sea level and ends the hike.
+
+Function Description
+
+Complete the countingValleys function in the editor below.
+
+countingValleys has the following parameter(s):
+
+int steps: the number of steps on the hike
+string path: a string describing the path
+Returns
+
+int: the number of valleys traversed
+Input Format
+
+The first line contains an integer , the number of steps in the hike.
+The second line contains a single string , of  characters that describe the path.
+
+Constraints
+
+Sample Input
+
+8
+UDDDUDUU
+Sample Output
+
+1
+Explanation
+
+If we represent _ as sea level, a step up as /, and a step down as \, the hike can be drawn as:
+
+_/\      _
+   \    /
+    \/\/
+The hiker enters and leaves one valley.
+
+Language
+C
+More
+2526272829303132333435363738394041424344454647484950
+                in_valley = 1;
+            current_level--;
+        }
+    }
+    return valleys;
+
+}
+
+int main()
+{
+
+Line: 45 Col: 20
+
+Test against custom input
+Problem Solving
+You have earned 15.00 points!
+You are now 94 points away from the 3rd star for your problem solving badge.
+6%106/200
+Congratulations
+You solved this challenge. Would you like to challenge your friends?Share on FacebookShare on TwitterShare on LinkedIn
+
+Test case 0
+
+Test case 1
+
+Test case 2
+
+Test case 3
+
+Test case 4
+
+Test case 5
+
+Test case 6
+
+Test case 7
+
+Test case 8
+
+Test case 9
+
+Test case 10
+
+Test case 11
+
+Test case 12
+
+Test case 13
+
+Test case 14
+
+Test case 15
+
+Test case 16
+
+Test case 17
+
+Test case 18
+
+Test case 19
+
+Test case 20
+
+Test case 21
+Compiler Message
+Success
+Input (stdin)
+8
+UDDDUDUU
+Expected Output
+1
+*/
+
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+int countingValleys(int steps, char* path) {
+     int current_level = 0;
+    int valleys = 0;
+    int in_valley = 0; // Flag to indicate if currently in a valley
+    
+    for (int i = 0; i < steps; i++) {
+        if (path[i] == 'U') {
+            current_level++;
+            if (current_level == 0 && in_valley) {
+                valleys++;
+                in_valley = 0;
+            }
+        } else {
+            if (current_level == 0)
+                in_valley = 1;
+            current_level--;
+        }
+    }
+    return valleys;
+
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int steps = parse_int(ltrim(rtrim(readline())));
+
+    char* path = readline();
+
+    int result = countingValleys(steps, path);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}

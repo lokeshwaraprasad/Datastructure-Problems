fdafe430b2341cdbf1b7715197d1394f1fe21d2a
Lokeshwaraprasad <venkatesanlokeshwaraprasad@gmail.com>
2024-05-15 21:18:19 +0530

    Divisible sum pair#problemsolving#HACKERRANK.C

---
+++ divisiblesumpair.c
@@ -0,0 +1,76 @@
+/*
+Given an array of integers and a positive integer , determine the number of  pairs where  and  +  is divisible by .
+
+Example
+
+
+
+Three pairs meet the criteria:  and .
+
+Function Description
+
+Complete the divisibleSumPairs function in the editor below.
+
+divisibleSumPairs has the following parameter(s):
+
+int n: the length of array 
+int ar[n]: an array of integers
+int k: the integer divisor
+Returns
+- int: the number of pairs
+
+Input Format
+
+The first line contains  space-separated integers,  and .
+The second line contains  space-separated integers, each a value of .
+
+Constraints
+
+Sample Input
+
+STDIN           Function
+-----           --------
+6 3             n = 6, k = 3
+1 3 2 6 1 2     ar = [1, 3, 2, 6, 1, 2]
+Sample Output
+
+ 5
+Explanation
+
+Here are the  valid pairs when :
+
+
+*/
+
+#include <stdio.h>
+int numberofpair(int arr[], int n)
+{
+    int count = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if ((arr[i] + arr[j]) % 3 == 0)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
+int main()
+{
+    int n;
+    printf("enter the size of the array:");
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+int a=numberofpair(arr,n);
+printf("\n%d",a);
+}
